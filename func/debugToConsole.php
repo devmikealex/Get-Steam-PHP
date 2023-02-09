89a6948d6167eaba4d00882bee5a4fa5fd8419c9
Mike Alex <devmikealex@gmail.com>
2023-02-09 14:16:31 +0300

    Update debug_to_console and add test

--- func/debugToConsole.php
+++ func/debugToConsole.php
@@ -1,15 +1,30 @@
 <?php
 
-// TODO Надо доделать для печати объектов
-function debug_to_console($data, $title='')
+function debug_to_console($output, $title = '')
 {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
-    if (is_object($output))
-        $output = print_r($output, true);
-    // echo "<script>console.log('PHPDebug> " . $output . "' );</script>";
+    // if (is_array($output))
+    // $output = implode(',', $output);
+    if (is_object($output) || is_array($output)) {
+        $output = preg_replace('~[\r\n]+~', '\n', print_r($output, true));
+    }
     echo <<<SCRIPT
     <script>console.log('PHPDebug > $title : $output');</script>
     SCRIPT;
+}
+
+// TestDTC();
+
+function TestDTC()
+{
+    $a = 10;
+    $b = 'Adasd a ad asd';
+    $c = [1, 2, 3, 4];
+    $d = ["red" => "красный", "blue" => "синий", "green" => "зеленый"];
+    $e = (object) array('1' => 'foo', '2' => 'booo');
+    debug_to_console($a, 'A');
+    debug_to_console($b, 'B');
+    debug_to_console($c, 'C');
+    debug_to_console($d, 'D');
+    debug_to_console($e, 'E');
+    die('Test end');
 }
